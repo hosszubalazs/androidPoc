30463dfb55d04b593054dbb5e84065b1454e8b4e
root <hbaloo28@gmail.com>
2017-01-30 22:32:42 +0100

    Commit inital state

---
+++ app/src/main/java/com/example/balazslocal/poc/CurrentPos.java
@@ -0,0 +1,10 @@
+package com.example.balazslocal.poc;
+
+/**
+ * Created by BalazsLocal on 2016. 12. 30..
+ */
+
+public class CurrentPos {
+    public double LAT;
+    public double LONG;
+}
---
+++ app/src/main/java/com/example/balazslocal/poc/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.balazslocal.poc;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.content.Context;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    CurrentPos pos;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.binding_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+       String[] INITIAL_PERMS={
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.READ_CONTACTS
+        };
+
+        requestPermissions(INITIAL_PERMS, 101);
+
+        //MainActivity binding = DataBindingUtil.setContentView(this, R.layout.main_activity);
+        //binding.setUser(user);
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch(requestCode) {
+            case 101:
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                checkPermission(Manifest.permission.ACCESS_FINE_LOCATION,101,01);
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // TODO Auto-generated method stub
+
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        String log ="Latitude: " + latitude + ", Longitude: " + longitude;
+        //TextView t = (TextView) findViewById(R.id.textView);
+        //t.setText(log);
+        Log.i("Location",log);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
+    }
+}
